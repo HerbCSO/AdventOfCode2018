de9997ac4bef46da4d69df48c376ed2abbfbf19c
Carsten Dreesbach <carsten.dreesbach@opower.com>
2018-12-02 10:29:32 -0500

    Day 2-1: Yeah, OK, it works, but it's kinda ugly

---
+++ day2-1.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+counts2 = 0
+counts3 = 0
+open(ARGV[0]) do |file|
+  file.each do |line|
+    letters = Hash.new { |k, v| k = k; v = 0 }
+    line.scan(/./) do |char|
+      letters[char] += 1
+    end
+    counts2 += 1 if letters.any? { |k, v| v == 2 }
+    counts3 += 1 if letters.any? { |k, v| v == 3 }
+  end
+end
+puts "Checksum = #{counts2 * counts3}"
