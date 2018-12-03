6796aac15b769da115e92bdd51365e3dc4dfcd5f
Carsten Dreesbach <carsten.dreesbach@opower.com>
2018-12-03 16:45:52 -0500

    Day 3: solutions 1 and 2

---
+++ day3/day3-1.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+def parse_line(line)
+  # puts line
+  nil if line.to_s.strip.empty?
+  claim_number, x_pos, y_pos, x_length, y_length = line.split(/ @ |,|: |x/)
+  [claim_number, x_pos.to_i, y_pos.to_i, x_length.to_i, y_length.to_i]
+end
+
+def fill_fabric(fabric, claim_number, x_pos, y_pos, x_length, y_length)
+  # puts x_pos+x_length
+  # puts y_pos+y_length
+  (x_pos...x_pos+x_length).each do |x|
+    (y_pos...y_pos+y_length).each do |y|
+      # puts "#{x}, #{y}, #{claim_number}"
+      fabric[[x, y]] << claim_number
+      # pp fabric
+    end
+  end
+end
+
+fabric = Hash.new { |h, k| h[k] = [] }
+open(ARGV[0]) do |file|
+  file.each do |line|
+    claim_number, x_pos, y_pos, x_length, y_length = parse_line(line)
+    # puts "#{claim_number}, #{x_pos}, #{y_pos}, #{x_length}, #{y_length}"
+    fill_fabric(fabric, claim_number, x_pos, y_pos, x_length, y_length)
+  end
+end
+pp fabric.take(100)
+puts fabric.select { |coords, assigned| assigned.length > 1 }.length
---
+++ day3/day3-2.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+def parse_line(line)
+  # puts line
+  nil if line.to_s.strip.empty?
+  claim_number, x_pos, y_pos, x_length, y_length = line.split(/ @ |,|: |x/)
+  [claim_number, x_pos.to_i, y_pos.to_i, x_length.to_i, y_length.to_i]
+end
+
+def fill_fabric(fabric, claim_number, x_pos, y_pos, x_length, y_length)
+  # puts x_pos+x_length
+  # puts y_pos+y_length
+  (x_pos...x_pos+x_length).each do |x|
+    (y_pos...y_pos+y_length).each do |y|
+      # puts "#{x}, #{y}, #{claim_number}"
+      fabric[[x, y]] << claim_number
+      # pp fabric
+    end
+  end
+end
+
+fabric = Hash.new { |h, k| h[k] = [] }
+open(ARGV[0]) do |file|
+  file.each do |line|
+    claim_number, x_pos, y_pos, x_length, y_length = parse_line(line)
+    # puts "#{claim_number}, #{x_pos}, #{y_pos}, #{x_length}, #{y_length}"
+    fill_fabric(fabric, claim_number, x_pos, y_pos, x_length, y_length)
+  end
+end
+assignments = fabric.values.flatten.uniq
+dupes = fabric.select { |coords, assigned| assigned.length > 1 }.values.flatten.uniq
+puts assignments - dupes
